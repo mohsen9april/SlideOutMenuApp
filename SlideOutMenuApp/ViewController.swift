c25f19d7b88654f5c5b4ddc8d97dcca5fa6b95f9
Mohsen Abdollahi <mohsen9april@gmail.com>
2019-07-24 14:15:36 +0430

    Initial Commit

---
+++ SlideOutMenuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenuApp
+//
+//  Created by Mohsen Abdollahi on 7/24/19.
+//  Copyright © 2019 Mohsen Abdollahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
